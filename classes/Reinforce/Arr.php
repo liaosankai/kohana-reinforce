87f8670aa3da5c1c3ddcae2857cff7eda1205af7
Liao San-Kai <liaosankai@gmail.com>
2014-04-21 08:52:54 +0800

    Arr::column 仿 5.5 array_column 追加

--- classes/Reinforce/Arr.php
+++ classes/Reinforce/Arr.php
@@ -89,7 +89,7 @@ class Reinforce_Arr extends Kohana_Arr
 
         $key_parts = explode('.', $key);
 
-        if (!is_array($array) or !array_key_exists($key_parts[0], $array)) {
+        if (!is_array($array) or ! array_key_exists($key_parts[0], $array)) {
             return false;
         }
 
@@ -116,6 +116,91 @@ class Reinforce_Arr extends Kohana_Arr
     public static function in_array($needle, $haystack, $case_insensitive)
     {
         return in_array(strtolower($needle), array_map('strtolower', $haystack));
+    }
+
+    /**
+     * The in_arrayi function is a case-insensitive version of in_array.
+     *
+     * @param string  $needle
+     * @param array  $haystack
+     * @param boolean $case_insensitive
+     * @return boolean
+     */
+    public static function column($input = null, $columnKey = null, $indexKey = null)
+    {
+        if (function_exists('array_column')) {
+            return array_column($input, $columnKey, $indexKey);
+        }
+        // Using func_get_args() in order to check for proper number of
+        // parameters and trigger errors exactly as the built-in array_column()
+        // does in PHP 5.5.
+        $argc = func_num_args();
+        $params = func_get_args();
+
+        if ($argc < 2) {
+            trigger_error("array_column() expects at least 2 parameters, {$argc} given", E_USER_WARNING);
+            return null;
+        }
+
+        if (!is_array($params[0])) {
+            trigger_error('array_column() expects parameter 1 to be array, ' . gettype($params[0]) . ' given', E_USER_WARNING);
+            return null;
+        }
+
+        if (!is_int($params[1]) && !is_float($params[1]) && !is_string($params[1]) && $params[1] !== null && !(is_object($params[1]) && method_exists($params[1], '__toString'))
+        ) {
+            trigger_error('array_column(): The column key should be either a string or an integer', E_USER_WARNING);
+            return false;
+        }
+
+        if (isset($params[2]) && !is_int($params[2]) && !is_float($params[2]) && !is_string($params[2]) && !(is_object($params[2]) && method_exists($params[2], '__toString'))
+        ) {
+            trigger_error('array_column(): The index key should be either a string or an integer', E_USER_WARNING);
+            return false;
+        }
+
+        $paramsInput = $params[0];
+        $paramsColumnKey = ($params[1] !== null) ? (string) $params[1] : null;
+
+        $paramsIndexKey = null;
+        if (isset($params[2])) {
+            if (is_float($params[2]) || is_int($params[2])) {
+                $paramsIndexKey = (int) $params[2];
+            } else {
+                $paramsIndexKey = (string) $params[2];
+            }
+        }
+
+        $resultArray = array();
+
+        foreach ($paramsInput as $row) {
+
+            $key = $value = null;
+            $keySet = $valueSet = false;
+
+            if ($paramsIndexKey !== null && array_key_exists($paramsIndexKey, $row)) {
+                $keySet = true;
+                $key = (string) $row[$paramsIndexKey];
+            }
+
+            if ($paramsColumnKey === null) {
+                $valueSet = true;
+                $value = $row;
+            } elseif (is_array($row) && array_key_exists($paramsColumnKey, $row)) {
+                $valueSet = true;
+                $value = $row[$paramsColumnKey];
+            }
+
+            if ($valueSet) {
+                if ($keySet) {
+                    $resultArray[$key] = $value;
+                } else {
+                    $resultArray[] = $value;
+                }
+            }
+        }
+
+        return $resultArray;
     }
 
 }
